be989b850a5452e2446ba894d777a1980315cfb9
OhYee <oyohyee@oyohyee.com>
2020-07-06 01:23:43 +0800

    fix: fix for ant-tag color(#master)

--- plugin.js
+++ plugin.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { generateTheme, getLessVars, isValidColor } = require('antd-theme-generator');
 
+var cache = '';
 class Plugin {
   constructor(generator) {
     this.generator = generator;
@@ -50,7 +51,7 @@ module.exports = function generate(themeOptions) {
       if (!(await fs.existsSync(dir))) {
         await fs.mkdirSync(dir);
       }
-      await generateTheme({
+      const temp = await generateTheme({
         antDir,
         antdStylesDir,
         stylesDir,
@@ -60,6 +61,40 @@ module.exports = function generate(themeOptions) {
         themeVariables,
         customColorRegexArray,
       });
+        if (temp !== cache) {
+            cache = temp;
+        await fs.appendFileSync(
+          outputFilePath,
+          `
+.ant-tag-pink {  color: #eb2f96;  background: #fff0f6;  border-color: #ffadd2;}
+.ant-tag-pink-inverse {color: #fff;background: #eb2f96;border-color: #eb2f96;}
+.ant-tag-magenta {color: #eb2f96;  background: #fff0f6;  border-color: #ffadd2;}
+.ant-tag-magenta-inverse {color: #fff;background: #eb2f96;border-color: #eb2f96;}
+.ant-tag-red {color: #f5222d;background: #fff0f6;border-color: #ffa39e;}
+.ant-tag-red-inverse {color: #fff;background: #f5222d;border-color: #f5222d;}
+.ant-tag-volcano {color: #fa541c;background: #fff2e8;border-color: #ffbb96;}
+.ant-tag-volcano-inverse {color: #fff;background: #fa541c;border-color: #fa541c;}
+.ant-tag-orange {color: #fa8c16;background: #fff7e6;border-color: #ffd591;}
+.ant-tag-orange-inverse {color: #fff;background: #fa8c16;border-color: #fa8c16;}
+.ant-tag-yellow {color: #fadb14;background: #feffe6;border-color: #fffb8f;}
+.ant-tag-yellow-inverse {color: #fff;background: #fadb14;border-color: #fadb14;}
+.ant-tag-gold {color: #faad14;background: #fffbe6;border-color: #ffe58f;}
+.ant-tag-gold-inverse {color: #fff;background: #faad14;border-color: #faad14;}
+.ant-tag-cyan {color: #13c2c2;background: #e6fffb;border-color: #87e8de;}
+.ant-tag-cyan-inverse {color: #fff;background: #13c2c2;border-color: #13c2c2;}
+.ant-tag-lime {color: #a0d911;background: #fcffe6;border-color: #eaff8f;}
+.ant-tag-lime-inverse {color: #fff;background: #a0d911;border-color: #a0d911;}
+.ant-tag-green {color: #52c41a;background: #f6ffed;border-color: #b7eb8f;}
+.ant-tag-green-inverse {color: #fff;background: #52c41a;border-color: #52c41a;}
+.ant-tag-blue {color: #1890ff;background: #e6f7ff;border-color: #91d5ff;}
+.ant-tag-blue-inverse {color: #fff;background: #1890ff;border-color: #1890ff;}
+.ant-tag-geekblue {  color: #2f54eb;  background: #f0f5ff;  border-color: #adc6ff;}
+.ant-tag-geekblue-inverse {color: #fff;background: #2f54eb;border-color: #2f54eb;}
+.ant-tag-purple {color: #722ed1;  background: #f9f0ff;  border-color: #d3adf7;}
+.ant-tag-purple-inverse {color: #fff;background: #722ed1;border-color: #722ed1;}
+`,
+        );
+      }
     } catch (e) {
       console.error(e);
     }
