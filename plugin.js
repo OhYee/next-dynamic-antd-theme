f11f9d37a3b89398d2ef5bc8c18c598002efa9f0
OhYee <oyohyee@oyohyee.com>
2020-10-21 18:10:48 +0800

    fix: add prefix params when user has a non-default prefix(#master)

--- plugin.js
+++ plugin.js
@@ -28,6 +28,7 @@ module.exports = function generate(themeOptions) {
     outputFilePath,
     lessFilePath = '/_next/static/color.less', // Use if outputFilePath is `path.join(__dirname, './.next/static/color.less'),`
     lessJSPath = 'https://cdnjs.cloudflare.com/ajax/libs/less.js/3.11.3/less.min.js',
+    prefix = 'antd',
   } = themeOptions;
 
   console.log(outputFilePath);
@@ -70,32 +71,32 @@ module.exports = function generate(themeOptions) {
         await fs.appendFileSync(
           outputFilePath,
           `
-.ant-tag-pink {  color: #eb2f96;  background: #fff0f6;  border-color: #ffadd2;}
-.ant-tag-pink-inverse {color: #fff;background: #eb2f96;border-color: #eb2f96;}
-.ant-tag-magenta {color: #eb2f96;  background: #fff0f6;  border-color: #ffadd2;}
-.ant-tag-magenta-inverse {color: #fff;background: #eb2f96;border-color: #eb2f96;}
-.ant-tag-red {color: #f5222d;background: #fff0f6;border-color: #ffa39e;}
-.ant-tag-red-inverse {color: #fff;background: #f5222d;border-color: #f5222d;}
-.ant-tag-volcano {color: #fa541c;background: #fff2e8;border-color: #ffbb96;}
-.ant-tag-volcano-inverse {color: #fff;background: #fa541c;border-color: #fa541c;}
-.ant-tag-orange {color: #fa8c16;background: #fff7e6;border-color: #ffd591;}
-.ant-tag-orange-inverse {color: #fff;background: #fa8c16;border-color: #fa8c16;}
-.ant-tag-yellow {color: #fadb14;background: #feffe6;border-color: #fffb8f;}
-.ant-tag-yellow-inverse {color: #fff;background: #fadb14;border-color: #fadb14;}
-.ant-tag-gold {color: #faad14;background: #fffbe6;border-color: #ffe58f;}
-.ant-tag-gold-inverse {color: #fff;background: #faad14;border-color: #faad14;}
-.ant-tag-cyan {color: #13c2c2;background: #e6fffb;border-color: #87e8de;}
-.ant-tag-cyan-inverse {color: #fff;background: #13c2c2;border-color: #13c2c2;}
-.ant-tag-lime {color: #a0d911;background: #fcffe6;border-color: #eaff8f;}
-.ant-tag-lime-inverse {color: #fff;background: #a0d911;border-color: #a0d911;}
-.ant-tag-green {color: #52c41a;background: #f6ffed;border-color: #b7eb8f;}
-.ant-tag-green-inverse {color: #fff;background: #52c41a;border-color: #52c41a;}
-.ant-tag-blue {color: #1890ff;background: #e6f7ff;border-color: #91d5ff;}
-.ant-tag-blue-inverse {color: #fff;background: #1890ff;border-color: #1890ff;}
-.ant-tag-geekblue {  color: #2f54eb;  background: #f0f5ff;  border-color: #adc6ff;}
-.ant-tag-geekblue-inverse {color: #fff;background: #2f54eb;border-color: #2f54eb;}
-.ant-tag-purple {color: #722ed1;  background: #f9f0ff;  border-color: #d3adf7;}
-.ant-tag-purple-inverse {color: #fff;background: #722ed1;border-color: #722ed1;}
+.${prefix}-tag-pink {  color: #eb2f96;  background: #fff0f6;  border-color: #ffadd2;}
+.${prefix}-tag-pink-inverse {color: #fff;background: #eb2f96;border-color: #eb2f96;}
+.${prefix}-tag-magenta {color: #eb2f96;  background: #fff0f6;  border-color: #ffadd2;}
+.${prefix}-tag-magenta-inverse {color: #fff;background: #eb2f96;border-color: #eb2f96;}
+.${prefix}-tag-red {color: #f5222d;background: #fff0f6;border-color: #ffa39e;}
+.${prefix}-tag-red-inverse {color: #fff;background: #f5222d;border-color: #f5222d;}
+.${prefix}-tag-volcano {color: #fa541c;background: #fff2e8;border-color: #ffbb96;}
+.${prefix}-tag-volcano-inverse {color: #fff;background: #fa541c;border-color: #fa541c;}
+.${prefix}-tag-orange {color: #fa8c16;background: #fff7e6;border-color: #ffd591;}
+.${prefix}-tag-orange-inverse {color: #fff;background: #fa8c16;border-color: #fa8c16;}
+.${prefix}-tag-yellow {color: #fadb14;background: #feffe6;border-color: #fffb8f;}
+.${prefix}-tag-yellow-inverse {color: #fff;background: #fadb14;border-color: #fadb14;}
+.${prefix}-tag-gold {color: #faad14;background: #fffbe6;border-color: #ffe58f;}
+.${prefix}-tag-gold-inverse {color: #fff;background: #faad14;border-color: #faad14;}
+.${prefix}-tag-cyan {color: #13c2c2;background: #e6fffb;border-color: #87e8de;}
+.${prefix}-tag-cyan-inverse {color: #fff;background: #13c2c2;border-color: #13c2c2;}
+.${prefix}-tag-lime {color: #a0d911;background: #fcffe6;border-color: #eaff8f;}
+.${prefix}-tag-lime-inverse {color: #fff;background: #a0d911;border-color: #a0d911;}
+.${prefix}-tag-green {color: #52c41a;background: #f6ffed;border-color: #b7eb8f;}
+.${prefix}-tag-green-inverse {color: #fff;background: #52c41a;border-color: #52c41a;}
+.${prefix}-tag-blue {color: #1890ff;background: #e6f7ff;border-color: #91d5ff;}
+.${prefix}-tag-blue-inverse {color: #fff;background: #1890ff;border-color: #1890ff;}
+.${prefix}-tag-geekblue {  color: #2f54eb;  background: #f0f5ff;  border-color: #adc6ff;}
+.${prefix}-tag-geekblue-inverse {color: #fff;background: #2f54eb;border-color: #2f54eb;}
+.${prefix}-tag-purple {color: #722ed1;  background: #f9f0ff;  border-color: #d3adf7;}
+.${prefix}-tag-purple-inverse {color: #fff;background: #722ed1;border-color: #722ed1;}
 `,
         );
       }
