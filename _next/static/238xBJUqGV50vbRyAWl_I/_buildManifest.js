a0ce06748599d00d012019ab495b60bb5ee9e40c
OhYee <oyohyee@oyohyee.com>
2020-11-02 09:49:18 +0000

    Update gh-pages to output generated at 1ea4277

---
+++ _next/static/238xBJUqGV50vbRyAWl_I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b]}}("static\u002Fcss\u002Fstyles.8f4b937f.chunk.css","static\u002Fchunks\u002Fstyles.d5c26d427516738245ca.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
